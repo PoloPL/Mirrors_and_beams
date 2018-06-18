6e9f709f6c4a46f3c6f7d6baa4c6156940b85fff
PoloPL <35652898+PoloPL@users.noreply.github.com>
2018-06-18 23:28:42 +0200

    Update drawing.js

--- drawing.js
+++ drawing.js
@@ -11,7 +11,7 @@ function createBoard(game, root) {
 	game.svg_fields = [];
 	game.svg_clip_fields = [];
 
-	var svg_el = createSVG("svg", root, SVG_WIDTH, SVG_HEIGHT);
+	var svg_el = createSVG("svg", root, "makeDraggable(evt)", SVG_WIDTH, SVG_HEIGHT);
 
 	for(var i=0; i<game.n; ++i) {
 		var g_el = createSVG("g", svg_el);
@@ -23,6 +23,7 @@ function createBoard(game, root) {
 			var rect_el = createSVG("rect", g_el);
 			setAttribs(
 				rect_el,
+				//["onload",'makeDraggable(evt)'],
 				["x", STROKE_WIDTH + j * (STROKE_WIDTH + FIELD_WIDTH)],
 				["y", STROKE_WIDTH + i * (STROKE_WIDTH + FIELD_HEIGHT)],
 				["width", FIELD_WIDTH],
@@ -32,14 +33,14 @@ function createBoard(game, root) {
 		}
 	}
 
-	var g_el = createSVG("g", createSVG("svg", root, SVG_CLIP_WIDTH, SVG_CLIP_HEIGHT));
+	var g_el = createSVG("g", createSVG("svg", root, "makeDraggable(evt)", SVG_CLIP_WIDTH, SVG_CLIP_HEIGHT));
 	g_el.setAttribute("style", "stroke-width: " + STROKE_WIDTH + ";");
 	for(var i=0; i<game.clip_size; ++i) {
 		var rect_el = createSVG("rect", g_el);
 		setAttribs(
 			rect_el,
-				["x", STROKE_WIDTH + i * (STROKE_WIDTH + FIELD_WIDTH)],
-				["y", STROKE_WIDTH],
+				["x", STROKE_WIDTH + i * (STROKE_WIDTH + FIELD_WIDTH)+100],
+				["y", STROKE_WIDTH+495],
 				["width", FIELD_WIDTH],
 				["height", FIELD_HEIGHT]
 		);
@@ -50,7 +51,51 @@ function createBoard(game, root) {
 function updateBoard(game) {
 	for(var field of game.board) {
 		var svg_field = game.svg_fields[field.pos.x][field.pos.y];
+		svg_field.setAttribute("class", 'draggable');
 		svg_field.setAttribute("fill", field.col);
 	}
+}
+
+
+
+function makeDraggable(evt) {
+  var svg = evt.target;
+  svg.addEventListener('mousedown', startDrag);
+  svg.addEventListener('mousemove', drag);
+  svg.addEventListener('mouseup', endDrag);
+  svg.addEventListener('mouseleave', endDrag);
+  var selectedElement = false
+  
+  function getMousePosition(evt) {
+  var CTM = svg.getScreenCTM();
+  return {
+    x: (evt.clientX - CTM.e) / CTM.a,
+    y: (evt.clientY - CTM.f) / CTM.d
+  };
+}
+  
+var selectedElement, offset;
+function startDrag(evt) {
+  if (evt.target.classList.contains('draggable')) {
+    selectedElement = evt.target;
+    offset = getMousePosition(evt);
+    offset.x -= parseFloat(selectedElement.getAttributeNS(null, "x"));
+    offset.y -= parseFloat(selectedElement.getAttributeNS(null, "y"));
+  }
+}
+  
+function drag(evt) {
+  if (selectedElement) {
+    evt.preventDefault();
+    var coord = getMousePosition(evt);
+    selectedElement.setAttributeNS(null, "x", coord.x - offset.x);
+    selectedElement.setAttributeNS(null, "y", coord.y - offset.y);
+  }
+}
+
+function endDrag(evt) {
+  selectedElement = null;
+}
+
 }
 
